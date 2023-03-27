c965171ed9fa5e8a8091762fbc1fa30d641fe7c6
FaithKamau <faith.kamau@student.moringascgool.com>
2023-03-27 10:34:52 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+  }
+  function subtract(c, d) {
+    return d + c;
+  }
+  function divide(e, f) {
+    return e + f;
+  }
+  function multiply(g, h) {
+    return h + g;
+  }
+
+
+  
+let y = 5;
+
+// Use the prefix increment operation
+let prefix = y++;
+
+console.log(postfix);
+    
+  // Set a variable
+  let a = 7;
+
+  // Use the prefix decrement operation
+  
+  a--;
+  
+  console.log(a);
+
+  var input = prompt('Enter a number');
+var makeIntString = makeIntString (input);
+console.log(parseInt(input));
+
+
+
+var preserveDecimal(string) = parseFloat("554,20".replace(",", "."));
+console.log(parseFloat(preserveDecimal));
